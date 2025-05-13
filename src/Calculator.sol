6256ca64812dc93224af62cf5833aad9ef49ba8d
ralphmcralph <rhmenm@gmail.com>
2025-05-13 21:30:08 +0200

    Unit and fuzzing testing

--- src/Calculator.sol
+++ src/Calculator.sol
@@ -3,58 +3,59 @@
 pragma solidity 0.8.24;
 
 contract Calculator {
-   
-   // Variables
-   uint256 public resultado;
-   address public admin;
+    // Variables
+    uint256 public resultado;
+    address public admin;
 
-   // Events
-   event Addition(uint256 num1_, uint256 num2_, uint256 resultado_);
-   event Substraction(uint256 num1_, uint256 num2_, uint256 resultado_);
-   event Multiplier(uint256 num1_, uint256 num2_, uint256 resultado_);
-   event Division(uint256 num1_, uint256 num2_, uint256 resultado_);
+    // Events
+    event Addition(uint256 num1_, uint256 num2_, uint256 resultado_);
+    event Substraction(uint256 num1_, uint256 num2_, uint256 resultado_);
+    event Multiplier(uint256 num1_, uint256 num2_, uint256 resultado_);
+    event Division(uint256 num1_, uint256 num2_, uint256 resultado_);
 
-   // Modifiers
-   modifier onlyAdmin {
+    // Modifiers
+    modifier onlyAdmin() {
         require(msg.sender == admin, "Not allowed");
         _;
-   }
+    }
 
-   constructor(uint256 firstResultado_, address admin_){
-    resultado = firstResultado_;
-    admin = admin_;
-   }
-   // Functions
-        // 1. Addition
-    function addition(uint256 num1_, uint256 num2_) external returns(uint256 resultado_) {
+    constructor(uint256 firstResultado_, address admin_) {
+        resultado = firstResultado_;
+        admin = admin_;
+    }
+    // Functions
+    // 1. Addition
+
+    function addition(uint256 num1_, uint256 num2_) external returns (uint256 resultado_) {
         resultado_ = num1_ + num2_;
         resultado = resultado_;
 
         emit Addition(num1_, num2_, resultado_);
     }
-        // 2. Substraction
-    function substraction(uint256 num1_, uint256 num2_) external returns(uint256 resultado_) {
+    // 2. Substraction
+
+    function substraction(uint256 num1_, uint256 num2_) external returns (uint256 resultado_) {
         resultado_ = num1_ - num2_;
         resultado = resultado_;
 
         emit Substraction(num1_, num2_, resultado_);
     }
-        // 3. Multiplier
-    function multiplier(uint256 num1_, uint256 num2_) external returns(uint256 resultado_) {
+    // 3. Multiplier
+
+    function multiplier(uint256 num1_, uint256 num2_) external returns (uint256 resultado_) {
         resultado_ = num1_ * num2_;
         resultado = resultado_;
 
         emit Multiplier(num1_, num2_, resultado_);
     }
-    
-        // 4. Division
 
-    function division(uint256 num1_, uint256 num2_) external onlyAdmin returns(uint256 resultado_) {
+    // 4. Division
+
+    function division(uint256 num1_, uint256 num2_) external onlyAdmin returns (uint256 resultado_) {
         if (num2_ == 0) return 0;
         resultado_ = num1_ / num2_;
         resultado = resultado_;
 
         emit Division(num1_, num2_, resultado_);
     }
-
 }
--- test/Calculator.t.sol
+++ test/Calculator.t.sol
@@ -6,7 +6,6 @@ import "../src/Calculator.sol";
 import "forge-std/Test.sol";
 
 contract CalculatorTest is Test {
-    
     Calculator calculator;
     uint256 public firstResult = 100;
     address public admin = vm.addr(1);
@@ -23,24 +22,24 @@ contract CalculatorTest is Test {
         assert(calculator.resultado() == firstResult);
     }
 
-    function testAddition() public {        
-        assert(calculator.addition(num1_, num2_) == num1_ + num2_);        
+    function testAddition() public {
+        assert(calculator.addition(num1_, num2_) == num1_ + num2_);
     }
 
-    function testSubstraction() public {        
-        assert(calculator.substraction(num1_, num2_) == num1_ - num2_);        
+    function testSubstraction() public {
+        assert(calculator.substraction(num1_, num2_) == num1_ - num2_);
     }
 
-    function testMultiplier() public {      
-        assert(calculator.multiplier(num1_, num2_) == num1_ * num2_);        
+    function testMultiplier() public {
+        assert(calculator.multiplier(num1_, num2_) == num1_ * num2_);
     }
 
     function testCannotMultiply2LargeNumbers() public {
         uint256 large1_ = 5;
-        uint256 large2_ = 2**256 - 2;
+        uint256 large2_ = 2 ** 256 - 2;
 
         vm.expectRevert();
-        calculator.multiplier(large1_, large2_);        
+        calculator.multiplier(large1_, large2_);
     }
 
     function testWhenNotAdminDivision() public {
@@ -64,7 +63,7 @@ contract CalculatorTest is Test {
         vm.expectRevert();
         console.log(msg.sender);
         calculator.division(num1_, num2_);
-    }  
+    }
 
     // Fuzzing testing (Advanced) - Random inputs
 
@@ -75,6 +74,4 @@ contract CalculatorTest is Test {
 
         vm.stopPrank();
     }
-
 }
-
